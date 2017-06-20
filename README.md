f6b4ab8b4a7e2e6c74341abd86a6e874534ac60f
Matthew Tran <Matthew@Matthews-MacBook-Air.local>
2017-06-20 08:58:04 -0700

    Search and button function correctly pulls searched city

---
+++ README.md
@@ -0,0 +1,10 @@
+# Weather App
+
+Thank you for checking out this project!
+
+## Getting Started
+
+* Download and install reload
+* Clone this repository
+* Run Bower install
+* Run reload .
--- app/weather.controller.js
+++ app/weather.controller.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     'use strict';
 
     angular
@@ -8,21 +8,26 @@
     weatherController.$inject = ['$http'];
 
     function weatherController($http) {
-        /* jshint validthis:true */
         var vm = this;
-        vm.message = 'Hello';
+        vm.getWeather = getWeather;
 
-        $http
-            .get('http://api.openweathermap.org/data/2.5/weather?q=sandiego&units=imperial&apikey=c290bc6fa7278451b2d24d29650191bb')
-            .then(function (response) {
-                vm.weatherInfo = response.data;
-                console.log(vm.weatherInfo.name);
-            });
+        ///////////////////////////////////////////////
+
+        function getWeather(term) {
+
+            $http
+                .get('http://api.openweathermap.org/data/2.5/weather?q=' + term + '&units=imperial&apikey=5e9f84a8d383be947ca6f260e042187f')
+                .then(function (response) {
+                    vm.weatherInfo = response.data;
+                    console.log(vm.weatherInfo.name);
+                });
+
+        }
 
 
 
         activate();
 
-        function activate() { }
+        function activate() {}
     }
 })();
